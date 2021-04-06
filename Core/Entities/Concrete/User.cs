5c3dfe4d6d2fd97f1394ba963ce6bdde8fa699e7
Emirhan6 <70206176+Emirhan6@users.noreply.github.com>
2021-04-06 17:29:48 +0300

    Core Commit

---
+++ Core/Entities/Concrete/User.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class User : IEntity
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EMail { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public bool Status { get; set; }
+    }
+}
