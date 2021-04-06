17affd02ac59ec09f7eac4454f3cfd07aeaf7677
Emirhan6 <70206176+Emirhan6@users.noreply.github.com>
2021-04-06 17:44:39 +0300

    DataAccess Commit

---
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,69 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarDal : EfEntityRepositoryBase<Car,RecapProjectDatabaseContext>,ICarDal
+    {
+        public List<CarDetailDto> GetCarDetails(Expression<Func<Car, bool>> filter = null)
+        {
+            using (RecapProjectDatabaseContext context = new RecapProjectDatabaseContext())
+            {
+                var result = from ca in filter is null ? context.Cars : context.Cars.Where(filter)
+                             join co in context.Colors
+                             on ca.ColorId equals co.ColorId
+                             join br in context.Brands
+                             on ca.BrandId equals br.BrandId
+                             join img in context.CarImages
+                             on ca.CarId equals img.CarId
+                             select new CarDetailDto
+                             {
+                                 CarId = ca.CarId,
+                                 BrandId = br.BrandId,
+                                 BrandName = br.BrandName,
+                                 CarName = ca.CarName,
+                                 ColorId = co.ColorId,
+                                 ColorName = co.ColorName,
+                                 DailyPrice = ca.DailyPrice,
+                                 Descriptions = ca.Descriptions,
+                                 ModelYear = ca.ModelYear,
+                                 CarImageDate=img.CarImageDate,
+                                 ImagePath=img.ImagePath
+                             };
+
+                return result.ToList();
+            }
+        }
+
+        public List<CarDetailDto> GetCarDetailsById(int carId)
+        {
+            using (RecapProjectDatabaseContext context = new RecapProjectDatabaseContext())
+            {
+                var result = (from ca in context.Cars
+                              join co in context.Colors on ca.ColorId equals co.ColorId
+                              join br in context.Brands on ca.BrandId equals br.BrandId
+                              where ca.CarId == carId
+                              select new CarDetailDto
+                              {
+                                  CarId = ca.CarId,
+                                  CarName = ca.Descriptions,
+                                  BrandId = br.BrandId,
+                                  DailyPrice = ca.DailyPrice,
+                                  BrandName = br.BrandName,
+                                  ColorId = co.ColorId,
+                                  ColorName = co.ColorName,
+                                  ModelYear = ca.ModelYear
+                              }).ToList();
+                return result.ToList();
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfRentalDal.cs
@@ -0,0 +1,39 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfRentalDal:EfEntityRepositoryBase<Rental,RecapProjectDatabaseContext>,IRentalDal
+    {
+        public List<RentalDetailDto> GetCarDetails(Expression<Func<Rental,bool>> filter = null)
+        {
+            using (RecapProjectDatabaseContext context = new RecapProjectDatabaseContext())
+            {
+                var result = from r in context == null ? context.Rentals : context.Rentals.Where(filter)
+                             join c in context.Cars
+                             on r.CarId equals c.CarId
+                             join cu in context.Customers
+                             on r.CustomerId equals cu.CustomerId
+                             join b in context.Brands
+                             on c.BrandId equals b.BrandId
+                             select new RentalDetailDto
+                             {
+                    
+                                 RentalId = r.RentalId,
+                                 CarName = b.BrandName,
+                                 CustomerName = cu.CustomerName,
+                                 RentDate = r.RentDate,
+                                 ReturnDate = r.ReturnDate
+                             };
+                return result.ToList();
+            }
+        }
+    }
+}
