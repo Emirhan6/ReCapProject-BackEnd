dc16f9cae093295871a2e773489ab960e0bcd65c
Emirhan6 <70206176+Emirhan6@users.noreply.github.com>
2021-04-11 23:59:38 +0300

    Add files via upload

---
+++ Entities/Concrete/CreditCard.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class CreditCard : IEntity
+    {
+        public int CreditCardId { get; set; }
+        public int UserId { get; set; }
+        public int CardNumber { get; set; }
+        public string FullName { get; set; }
+    }
+}
---
+++ Entities/Concrete/Findeks.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Findeks : IEntity
+    {
+        public int FindeksId { get; set; }
+        public int CustomerId { get; set; }
+        public int FindeksPoint { get; set; }
+    }
+}
