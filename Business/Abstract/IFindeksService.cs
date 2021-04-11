75151c68e80779218bb75459503cf1a63c17adc2
Emirhan6 <70206176+Emirhan6@users.noreply.github.com>
2021-04-11 23:55:24 +0300

    Add files via upload

---
+++ Business/Abstract/IFindeksService.cs
@@ -0,0 +1,23 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IFindeksService
+    {
+        IResult Add(Findeks findeks);
+
+        IResult Update(Findeks findeks);
+
+        IResult Delete(Findeks findeks);
+        IDataResult<Findeks> GetById(int id);
+
+        IDataResult<Findeks> GetByCustomerId(int customerId);
+
+        IDataResult<List<Findeks>> GetAll();
+        IDataResult<Findeks> TotalFindeksPoint(Findeks findeks);
+    }
+}
